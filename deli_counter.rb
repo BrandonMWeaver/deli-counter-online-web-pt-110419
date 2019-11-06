0bbaf867d6c83a048d9a47ee2b30419fce29466d
Brandon Weaver <a7xncob@gmail.com>
2019-11-06 02:06:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,15 +2,13 @@ katz_deli = []
 
 def line(customers)
   if (customers.size > 0)
-    puts "The line is currently:"
-    
     string = ""
     index = 1
     while index < customers.size do
       string += " #{index}. #{customers[index]}"
       index += 1
     end
-    puts string
+    puts "The line is currently: #{string}"
   else
     puts "The line is currently empty."
   end
